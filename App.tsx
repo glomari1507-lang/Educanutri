fda6ac57d1a93e2599a469d4096c8d3396e3f7dd
glomari1507-lang <glomari1507@gmail.com>
2026-06-16 20:25:30 -0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import FoodsPage from "./pages/FoodsPage";
+import MythsPage from "./pages/MythsPage";
+import QuizPage from "./pages/QuizPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="flex min-h-screen flex-col font-['Nunito']">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sobre" element={<AboutPage />} />
+            <Route path="/alimentos" element={<FoodsPage />} />
+            <Route path="/mitos" element={<MythsPage />} />
+            <Route path="/quiz" element={<QuizPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
