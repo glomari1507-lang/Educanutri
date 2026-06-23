f407b0a73e4878a6d0f25864e42e9cd46562b7c1
glomari1507-lang <glomari1507@gmail.com>
2026-06-23 19:02:27 -0300

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -34,7 +34,6 @@ export default function LoginPage() {
           password,
         });
         if (error) throw error;
-        // Redireciona para o perfil após o login
         navigate('/perfil');
       }
     } catch (error: any) {
